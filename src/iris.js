297be1701939f2d87be3f46e07451878bdd92b99
Michaël Zasso <targos@protonmail.com>
2019-08-02 14:47:47 +0200

    small fixes

--- src/iris.js
+++ src/iris.js
@@ -4,58 +4,37 @@ const dataset = require('./iris.json');
 const cvSetsK7 = require('./cvSetsK7.json');
 const cvSetsK5 = require('./cvSetsK5.json');
 
-/**
- * retrieve the full dataset
- * @return {Array} - dataset with features and labels
- */
-exports.getDataset = function () {
-    return dataset.slice();
-};
+function getDataset() {
+  return dataset.slice();
+}
 
-/**
- * retrieve only the features
- * @return {Array} - an array with the features
- */
-exports.getNumbers = function () {
-    return dataset.map(d => d.slice(0, 4));
-};
+function getNumbers() {
+  return dataset.map((d) => d.slice(0, 4));
+}
 
-/**
- * retrieve the labels (or classes)
- * @return {Array} - an array with labels as character
- */
-exports.getClasses = function () {
-    return dataset.map(d => d[4]);
-};
+function getClasses() {
+  return dataset.map((d) => d[4]);
+}
 
-/**
- * retrieve the labels tags (distinct classes)
- * @return {Array} - an array with labels tags
- */
-exports.getDistinctClasses = function () {
-    return ['setosa', 'versicolor', 'virginica'];
-};
+function getDistinctClasses() {
+  return ['setosa', 'versicolor', 'virginica'];
+}
 
-/**
- * retrieve cross-validation sets for testing purpose 
- * this is usefull to test supervised algorithms
- * @param {Number} k - the number of folds
- * @param {Object} [options]
- * @param {Number} [options.idx=0] - index of the cv sets, either 0 or 1
- * @param {Number} [options.by='folds'] - choose the format: either 'folds' or 'trainTest'
- */
-exports.getCrossValidationSets = function(k, options) {
-    const {idx = 0,
-    by = 'folds'} = options;
-    var res;
-    k == 7 ? res = cvSetsK7[idx] : [];
-    k == 5 ? res = cvSetsK5[idx] : [];
+function getCrossValidationSets(k, options) {
+  const { idx = 0, by = 'folds' } = options;
+  var res;
+  k === 7 ? (res = cvSetsK7[idx]) : [];
+  k === 5 ? (res = cvSetsK5[idx]) : [];
+  if (k !== 7 && k !== 5) {
+    throw new Error(`k must be 7 or 5`);
+  }
 
-switch(by) {
-    case 'folds': {
-      res = res.map((x) => x.map((y) => y - 1));
-    }
-    break;
+  switch (by) {
+    case 'folds':
+      {
+        res = res.map((x) => x.map((y) => y - 1));
+      }
+      break;
     case 'trainTest': {
       res = res.map((x) => x.map((y) => y - 1));
       let folds = [];
@@ -69,7 +48,19 @@ switch(by) {
         folds.push({ testIndex: testCv, trainIndex: cv });
       }
       res = folds;
+      break;
+    }
+    default: {
+      throw new Error(`unknown "by" option: ${by}`);
     }
   }
-    return res;
+  return res;
 }
+
+module.exports = {
+  getDataset,
+  getNumbers,
+  getClasses,
+  getDistinctClasses,
+  getCrossValidationSets,
+};
